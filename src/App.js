d3b60ce0cbdabd455be56e6ce7bda438cd639fea
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-03-22 12:59:53 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import useAPI from './hooks/useAPI'
+
+function App() {
+  return (
+    <div className="App App-header">
+      <h1>Custom API Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useAPI.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+const useAPI = () => {
+  return (
+    <div>useAPI</div>
+  )
+}
+
+export default useAPI
