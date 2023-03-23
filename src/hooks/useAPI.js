42fa0322bd5fd8af72c3a951322f9acb1fc1c48e
Luis Espinal <luisespinal294@gmail.com>
2023-03-23 11:43:12 -0400

    Added comments

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,14 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useAPI = (route, id) => {
   //fetch data
   useEffect(() => {
     const baseUrl = 'https://jsonplaceholder.typicode.com'
     fetch(`${baseUrl}/${route}/${id}`)
-      .then(response => console.log(response.json()))
+      .then(response => response.json())
+      .then(json => console.log(json))
       .catch(error => console.log(error))
+
+    // async/await
+    // const fetchData = async () => {
+    // const response = await fetch(`${baseUrl}/${route}/${id}`)
+    // const responseJSON = await response.json()
+    // console.log(responseJSON)
+    // }
+    // fetchData()
+
   }, [route, id])
-  
+
+
 
 
   return (
